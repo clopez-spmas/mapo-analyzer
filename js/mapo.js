778e9e1486e6f0cc0537ca25d4f9292ab3567cd2
clopez-spmas <clopez@spmas.es>
2026-08-13 09:45:58 +0200

    Alinear cálculo final MAPO con rangos de Hospitalización y validar entradas

--- js/mapo.js
+++ js/mapo.js
@@ -1,16 +1,22 @@
-/* MAPO Analyzer - cálculo independiente de OCRA */
+/* MAPO Analyzer - cálculo independiente de OCRA. */
 
 function calculateMapo({ op, nc, pc, fs, fa, fc, famb, ff }) {
-  if (op <= 0) throw new Error('El número de operadores (OP) debe ser mayor que 0.');
-  if (nc < 0 || pc < 0) throw new Error('NC y PC no pueden ser negativos.');
+  const values = { op, nc, pc, fs, fa, fc, famb, ff };
+  for (const [key, value] of Object.entries(values)) {
+    if (!Number.isFinite(Number(value))) throw new Error(`El valor ${key} no es válido.`);
+  }
 
-  const ncTerm = (nc / op) * fs;
-  const pcTerm = (pc / op) * fa;
-  const mapo = (ncTerm + pcTerm) * fc * famb * ff;
+  const OP = Number(op), NC = Number(nc), PC = Number(pc);
+  if (OP <= 0) throw new Error('El número de operadores (OP) debe ser mayor que 0.');
+  if (NC < 0 || PC < 0) throw new Error('NC y PC no pueden ser negativos.');
+
+  const ncTerm = (NC / OP) * Number(fs);
+  const pcTerm = (PC / OP) * Number(fa);
+  const mapo = (ncTerm + pcTerm) * Number(fc) * Number(famb) * Number(ff);
 
   let classification;
   if (mapo === 0) classification = 'Ausente';
-  else if (mapo <= 1.5) classification = 'Irrelevante';
+  else if (mapo <= 1.5) classification = 'Aceptable';
   else if (mapo <= 5) classification = 'Medio';
   else classification = 'Alto';
 
